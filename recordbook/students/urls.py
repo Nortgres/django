8e71a42cd036d7161b9cc8d56386c5d98bb16e80
Nortgres <sergtron@ya.ru>
2024-01-22 19:55:33 +0300

    add files

--- recordbook/students/urls.py
+++ recordbook/students/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index, groups, about
+
+from .views import index, groups, about, students, teachers, login
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='home'),
+    path('about/', about, name='about'),
+    path('students/', students, name='students'),
+    path('teachers/', teachers, name='teachers'),
+    path('login/', login, name='login'),
     path('groups/<slug:group>/', groups),
-    path('about/', about),
+
 ]
--- recordbook/students/views.py
+++ recordbook/students/views.py
@@ -3,12 +3,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Student
 
-menu = ["О сайте", "Студенты", "Преподаватели", "Войти"]
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Студенты", 'url_name': 'students'},
+        {'title': "Преподаватели", 'url_name': 'teachers'},
+        {'title': "Войти", 'url_name': 'login'}]
 
 def index(request):
     #return HttpResponse("Страница приложения students.")
     students = Student.objects.all()
-    return render(request, 'students/index.html', {'students': students, 'menu': menu, 'title': 'Главная страница'})
+    context = {
+        'students': students,
+        'menu': menu,
+        'title': 'Главная страница'
+    }
+    return render(request, 'students/index.html', context=context)
+    #return render(request, 'students/index.html', {'students': students, 'menu': menu, 'title': 'Главная страница'})
 def groups(request, group):
     if request.GET:
         print(request.GET)
@@ -19,3 +28,12 @@ def groups(request, group):
 
 def about(request):
     return render(request, 'students/about.html', {'menu': menu, 'title': 'О сайте'})
+
+def students(request):
+    return HttpResponse("Студенты")
+
+def teachers(request):
+    return HttpResponse("Преподаватели")
+
+def login(request):
+    return HttpResponse("Авторизация")
