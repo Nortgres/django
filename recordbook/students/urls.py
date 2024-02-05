1d6a7303459ea06f83ac862b1356be6083cfa2b2
Nortgres <sergtron@ya.ru>
2024-02-05 20:06:17 +0300

    add files

--- recordbook/students/urls.py
+++ recordbook/students/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 
 from .views import groups, about, students, teachers, login, show_student, addstudent, StudentHome, ShowStudent, \
-    AddStudent, RegisterUser
+    AddStudent, RegisterUser, LoginUser, logout_user
 
 urlpatterns = [
 ##    path('', index, name='home'),
@@ -11,10 +11,11 @@ urlpatterns = [
     #path('student/<slug:stud_slug>/', show_student, name='student'),
     path('student/<slug:stud_slug>/', ShowStudent.as_view(), name='student'),
     path('teachers/', teachers, name='teachers'),
-    path('login/', login, name='login'),
+    path('login/', LoginUser.as_view(), name='login'),
     path('groups/<slug:group>/', groups),
     #path('addstudent/', addstudent, name='addstudent'),
     path('addstudent/', AddStudent.as_view(), name='addstudent'),
     path('', StudentHome.as_view(), name='home'),
     path('register/', RegisterUser.as_view(), name='register'),
+    path('logout/', logout_user, name='logout'),
 ]
--- recordbook/students/views.py
+++ recordbook/students/views.py
@@ -1,11 +1,13 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import logout, login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render, get_object_or_404, redirect
 # Create your views here.
 from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
 
-from .forms import AddStudentForm, RegisterUserForm
+from .forms import AddStudentForm, RegisterUserForm, LoginUserForm
 from .models import Student
 from .utils import menu, DataMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -42,8 +44,8 @@ def students(request):
 def teachers(request):
     return HttpResponse("Преподаватели")
 
-def login(request):
-    return HttpResponse("Авторизация")
+#def login(request):
+#    return HttpResponse("Авторизация")
 
 def show_student(request, stud_slug):
     #return HttpResponse(f"Отображение студента с id = {stud_id}")
@@ -108,8 +110,28 @@ class RegisterUser(DataMixin, CreateView):
     form_class = RegisterUserForm
     template_name = 'students/register.html'
     success_url = reverse_lazy('login')
-
     def get_context_data(self,*, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         c_def = self.get_user_context(title="Регистрация")
         return {**context, **c_def}
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
+
+
+class LoginUser(DataMixin, LoginView):
+    form_class = LoginUserForm
+    template_name = 'students/login.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Авторизация")
+        return {**context, **c_def}
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
