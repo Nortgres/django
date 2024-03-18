b0008174db72e0d29a71ed9921e5920cd695d3b6
Nortgres <sergtron@ya.ru>
2024-03-18 19:56:20 +0300

    add files

--- recordbook/students/tests.py
+++ recordbook/students/tests.py
@@ -1,3 +1,29 @@
-# from django.test import TestCase
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+'''def calc(a, b, c):
+    if c == '+':
+        return a + b
+    if c == '-':
+        return a - b
+    if c == '*':
+        return a * b
+
+
+class LogicTestCase(TestCase):
+    def test_plus(self):
+        result = calc(5, 7, '+')
+        self.assertEqual(12, result)
+
+    def test_minus(self):
+        result = calc(5, 7, '-')
+        self.assertEqual(-2, result)'''
+
+
+class StudentsApiTestCase(APITestCase):
+    def test_get(self):
+        url = reverse('students-list')
+        print(f'{url=}')
+        response = self.client.get(url)
+        print(f'{response=}')
